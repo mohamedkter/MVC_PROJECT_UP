f1d0c5603a3ffb51d08459a3e0bdecea6c4507b2
MOhamedRifai <mom358995@gmail.com>
2023-05-10 02:43:55 +0300

    upload

---
+++ MVC_Rifay/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Rifay.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC_Rifay/Controllers/User_DataController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Rifay.Controllers
+{
+    public class User_DataController : Controller
+    {
+        public IActionResult Signup()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
